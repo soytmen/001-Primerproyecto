06b2c46276d8b48484f96a281c9957bec39adca3
soytmen <114388813+soytmen@users.noreply.github.com>
2022-12-05 10:39:50 +0100

    69696

--- Assets/Sripts/Colisions.cs
+++ Assets/Sripts/Colisions.cs
@@ -6,23 +6,31 @@ public class Colisions : MonoBehaviour
 {
     public int Points;
 
-
-    //contador +1 cuando ciges una moneda, si chocas con un objeto -1,si llegas a -10 game over.
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log($"Tu puntuaion inicial es 0");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
-        
+        if (other.gameObject.name.Contains("coin"))
+        {
+            Destroy(other.gameObject);
+            Points += 1;
+            Debug.Log($"Llevas {Points}");
+        }
+
+        if (other.gameObject.name.Contains("obstacle"))
+        {
+            Points --;
+            Debug.Log($"Llevas {Points}");
+        }
+
     }
 }
